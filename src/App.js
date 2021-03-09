5ef6564356dd1b9213a3be10ae1f72100f9c2da8
FarrukhGit <1803910@brunel.ac.uk>
2021-03-09 10:10:58 +0000

    Characters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import Header from "./components/ui/Header";
+import CharacterGrid from "./CharacterGrid";
 
 const App = () => {
-  return <div className="container"></div>;
+  const [items, setItems] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      console.log(result.data);
+      setItems(result.data);
+      setLoading(false);
+    };
+    fetchItems();
+  }, []);
+
+  return (
+    <div className="container">
+      <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
+    </div>
+  );
 };
 
 export default App;
