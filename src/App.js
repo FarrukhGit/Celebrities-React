fac2894f465717430b0fde25a91ff35fecc0525c
FarrukhGit <1803910@brunel.ac.uk>
2021-03-09 10:41:57 +0000

    Search and Spinner components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import Header from "./components/ui/Header";
-import CharacterGrid from "./CharacterGrid";
+import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from './components/ui/Search'
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(
-        `https://www.breakingbadapi.com/api/characters`
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
       );
 
       console.log(result.data);
@@ -19,11 +21,12 @@ const App = () => {
       setLoading(false);
     };
     fetchItems();
-  }, []);
+  }, [query]);
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)}/>
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
